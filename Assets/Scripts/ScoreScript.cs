e57c285c1b2d82178b75e8508f98433b3bf04252
Alicia Stott <as8885@nyu.edu>
2015-08-09 16:37:13 -0400

    score script started

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+
+	public float score = 0;
+	public Text scoreText;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+		scoreText.text = score.ToString ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
